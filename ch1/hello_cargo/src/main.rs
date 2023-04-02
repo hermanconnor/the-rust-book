e9bab0509d5db82e9799b89c7497187279289a0e
herman <hermanconnor11@gmail.com>
2023-04-02 00:26:02 -0400

    Create hello cargo project using cargo

---
+++ ch1/hello_cargo/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+/*
+We can create a new project using -> cargo new
+We can build a project using -> cargo build
+We can build and run a project in 1 step using -> cargo run
+We can build a project without producing a binary to check for errors using -> cargo check
+When your project is ready for release, you can compile it with optimizations using -> cargo build --release
+*/
