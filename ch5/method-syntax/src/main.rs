50b51c62d4353fcc0aa0978d1333e86f3660e7f6
herman <hermanconnor11@gmail.com>
2023-04-17 23:23:12 -0400

    Start section on methods

---
+++ ch5/method-syntax/src/main.rs
@@ -0,0 +1,26 @@
+// *** METHODS ***
+// Methods are similar to functions: we declare them with the fn keyword and a name, they can have parameters and a return value, and they contain some code that’s run when the method is called from somewhere else. Unlike functions, methods are defined within the context of a struct, or an enum, or a trait object. Their first parameter is always self, which represents the instance of the struct the method is being called on.
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+}
